27ec7b240bd520ef6f1e826416a48d99b1759909
Jarred Bishop <jarredbishop@gmail.com>
2015-08-07 11:44:03 +1200

    tweaks

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
 var css             = require('!style!css!sass!../scss/style.scss');
 var Sizzle          = require('sizzle');
 // var TweenMax        = require('gsap');
-
-var els = {
-  Earth: Sizzle('.earth')[0],
-  EarthTexture: Sizzle('.js-earth-texture')[0]
-};
-
-var play = function(){
-  TweenMax.to(els.EarthTexture, 2, {
-    left: -250,
-    ease: Linear.easeNone
-  });
-};
-
-// play();
